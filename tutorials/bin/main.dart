396fdff7a85c31e62a9663a60c3d8e60dc53e4c0
DaehunGwak <rhkrgns56@naver.com>
2023-10-27 22:05:42 +0900

    done function chapter

--- tutorials/bin/main.dart
+++ tutorials/bin/main.dart
@@ -1,9 +1,11 @@
 import 'package:tutorials/printing.dart' as printing;
 import 'package:tutorials/variables.dart' as variables;
 import 'package:tutorials/data_types.dart' as data_types;
+import 'package:tutorials/functions.dart' as functions;
 
 void main(List<String> arguments) {
-	data_types.dataTypesMain();
-  variables.variablesMain();
-  printing.printHelloWrold();
+  functions.functionsMain();
+	// data_types.dataTypesMain();
+  // variables.variablesMain();
+  // printing.printHelloWrold();
 }
---
+++ tutorials/lib/functions.dart
@@ -0,0 +1,68 @@
+void functionsMain() {
+  defaultFunction();
+  namedParamters();
+  optionalPositionalParameters();
+  qqOperator();
+}
+
+void defaultFunction() {
+  print(plusTest(2, 3));
+  print(plusTest(3.3, 4.4));
+  print(plusTest(3.3, 3));
+}
+
+num plusTest(num n1, num n2) => n1 + n2;
+
+void namedParamters() {
+  print(sayHello(age: 12, name: 'ordi'));
+  print(sayHello(name: 'ordi'));
+  // print(sayHello()); // `name` argument required error
+}
+
+String sayHello({
+  required String name,
+  int age = 10
+  }) => 'hello, $name($age)!';
+
+void optionalPositionalParameters() {
+  print(optionalSayHello('ordi'));
+}
+
+String optionalSayHello(String name, [int? age = 10]) => 
+    'optionally, $name($age)';
+
+void qqOperator() {
+  print(capitalizeName1(null));
+  print(capitalizeName2(null));
+  print(capitalizeNameQq(null));
+
+  // qq assign
+  String? name;
+  print(name?.toUpperCase()); // null return
+  name ??= 'ooooo'; // name 이 null 이라면 assign
+  print(name?.toUpperCase());
+}
+
+String capitalizeName1(String? name) {
+  if (name != null) {
+    return name.toUpperCase();
+  }
+  return 'ANONNYMOUS';
+}
+
+String capitalizeName2(String? name) =>
+    name != null ? name.toUpperCase() : 'ANON2';
+
+String capitalizeNameQq(String? name) =>
+    name?.toUpperCase() ?? 'ANONQQ';
+
+typedef ListOfInts = List<int>;
+
+void typedefMain() {
+  print(reverseList([1, 2 ,3]));
+}
+
+ListOfInts reverseList(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
